0171408ffedb02e06d687c0879887d6a774b041e
et41 <erenturan41@gmail.com>
2018-02-04 01:14:59 +0300

    Truth Check

---
+++ truthcheck.js
@@ -0,0 +1,23 @@
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  let cnt = 0;
+  for( let i = 0 ; i <collection.length ; i++){
+  let keys =	Object.keys(collection[i]);
+  	for(let key of keys){
+  		if(key == pre &&  collection[i][key] ){
+  		cnt++;
+  		}
+  	}
+  }
+  if(collection.length == cnt){
+  	return true;
+  }else{
+  	return false
+  }
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, 
+	{"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, 
+	{"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age"));
